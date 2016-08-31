d1fcec672da027ae0ec684d61039f0431ca914d6
githublqs <645625872@qq.com>
2016-08-31 20:25:27 +0800

    跟随 phoneGap中文网 视频教程学习 至angularjs  ngSanitize  ngRoute  ngAnimate插件（1）

--- app/src/main/assets/www/test/js/app.js
+++ app/src/main/assets/www/test/js/app.js
@@ -1,12 +1,121 @@
 /**
  * Created by Administrator on 2016/8/30.
  */
-var app = angular.module("myApp", ['myApp.rHelloFilter']);
-app.directive("hello",function(){
+var app = angular.module("myApp", ['myApp.rHelloFilter','ngRoute','ngStorage']
+
+    ,function ($provide) {
+    //console.log($provide);
+    $provide.provider('providerServices01',function () {
+        //可以在config里面配置的属性
+        this.name='';
+        this.age='';
+        this.$get=function () {
+            var that=this;
+            var _name='';
+            var service={};
+            service.setName=function(name){
+
+                _name=name;
+            }
+            service.getName=function(){
+
+                return _name;
+            }
+            service.getConfigName=function(){
+
+                return that.name+'age:'+that.age;
+            }
+
+            return service;
+        }
+    });
+    /*$provide.provider('providerServices02',function () {
+        this.$get=function () {
+            return   'this is providerServices01';
+        }
+    });
+    $provide.('factoryServices01',function(){
+        return {
+            message:'this is factoryServices01'
+        }
+
+    });
+    $provide.factory('factoryServices02',function(){
+
+        return 'this is factoryServices01  text';
+    });*/
+    $provide.service('serviceServices02',function(){
+            return 'this is serviceServices02 text';//与factory的区别 无法返回字符串
+        //  console.log(serviceServices02); Object {}
+    });
+}).provider('providerServices03',function () {
+    this.$get=function () {
+        return   'this is providerServices03';
+    }
+}).service('serviceServices03',function(){
+    return {
+        message:'this is serviceServices03'
+    }
+}).factory('factoryServices03',function(){
+    return {
+        message:'this is factoryServices03'
+    }
+}).config(["$provide","providerServices01Provider",function ($provide,providerServices01Provider) {
+    $provide.service('serviceServices01',["$http","$rootScope","$log","$filter",function($http,$rootScope,$log,$filter){
+        var  _name;
+        this.setName=function (name) {
+            _name=name;
+        }
+        this.getName=function () {
+            return _name;
+        }
+        this.getData=function () {
+            $log.warn("log service warn");
+            var  myUrl ="http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1&callback=JSON_CALLBACK";
+            return $http.jsonp(myUrl,{cache:true})/*
+                .success(function (data) {
+                    console.log("-----getData----success-----");
+                    console.log(data);
+
+                }).error(function (err) {
+                    console.log("-----getData----error-----");
+                    console.log(err);
+                })*/;
+        }
+    }]);
+    providerServices01Provider.name="xxxxx";
+    providerServices01Provider.age=30;
+}]).config(["$interpolateProvider",function ($interpolateProvider) {
+    /*$interpolateProvider.startSymbol('##');
+    $interpolateProvider.endSymbol('##');*/
+}]).config(['$routeProvider',function($routeProvider){
+    $routeProvider
+        .when('/div1',{
+            templateUrl : 'template1.html',
+            controller : 'div1Controller'
+        })
+        .when('/div2',{
+            template : '<p>这是div2{{text}}</p>',
+            controller : 'div2Controller'
+        })
+        .when('/div3',{
+            template : '<p>这是div3{{text}}</p>',
+            controller : 'div3Controller'
+        })
+       .when('/content/:id/:cateid',{
+            template : '<p>这是content{{id}}</p>',
+            controller : 'div4Controller'
+        })
+        .otherwise({
+            redirectTo : '/div1'
+        });
+
+}])
+.directive("hello",function(){
     return{
         restrict:'E',
         template:'<div class="red">hello 2131313<strong>你好</strong>everyone</div>',
         replace:true
     }
 
-})
+});
--- app/src/main/assets/www/test/js/controllers.js
+++ app/src/main/assets/www/test/js/controllers.js
@@ -1,7 +1,30 @@
 /**
  * Created by lqs on 2016/8/30.
  */
-app.controller("personController",["$scope","$filter",function($scope,$filter) {
+
+app.controller("personController",["$scope","$filter","providerServices03","serviceServices03","factoryServices03","providerServices01","serviceServices02","serviceServices01","$location",'$anchorScroll',"$sce","$cacheFactory","$localStorage",
+  function ($scope,$filter,providerServices03,serviceServices03,factoryServices03,providerServices01,serviceServices02,serviceServices01,$location,$anchorScroll,$sce,$cacheFactory,$localStorage) {
+      //$location.search({'age':20});//跳转到http://localhost:63342/www/test/test1.html?_ijt=iephel07fs244rr67au1ktt9e8#?age=20
+      //console.log($location.absUrl());//http://localhost:63342/www/test/test1.html?_ijt=iephel07fs244rr67au1ktt9e8
+      //$location.hash('hello');//$anchorScroll 服务 则会跳转到  $$absUrl: "http://localhost:63342/www/test/test1.html?_ijt=iephel07fs244rr67au1ktt9e8##hello" 并且定位到 id为 hello 的元素,锚链接效果
+      //console.log($location.host());//localhost
+      //console.log($location.path("/new"));// 跳转到http://localhost:63342/www/test/test1.html?_ijt=iephel07fs244rr67au1ktt9e8#/new#hello //后退键可返回
+      //$location.path("/new").replace();//跳转到 http://localhost:63342/www/test/test1.html?_ijt=iephel07fs244rr67au1ktt9e8#/new#hello 无法后退返回
+      //console.log($location.protocol());//返回 http
+
+      var cache=$cacheFactory("cacheFactoryId");
+      cache.put("name", "lqs");
+      $localStorage.$default({
+          counter: 42
+      });
+
+
+      $scope.trustAsHtml=$sce.trustAsHtml('<h1>trustAsHtml</h1>');
+      $scope.trustAsHtmlFunc=function(){
+        return $sce.trustAsHtml('<h1>trustAsHtml</h1>');
+      };
+
+
     $scope.person= {
         firstName:"John2",
         lastName : "Doe1",
@@ -13,7 +36,7 @@ app.controller("personController",["$scope","$filter",function($scope,$filter) {
     };
    // $scope.personCopy=angular.copy($scope.person);
     //console.log(rHello);
-    var arrayObj = new Array();
+   // var arrayObj = new Array();
     //arrayObj.push(personCopy);
     /*arrayObj[0]=$scope.personCopy;
     arrayObj[1]=angular.copy($scope.person);
@@ -21,14 +44,38 @@ app.controller("personController",["$scope","$filter",function($scope,$filter) {
     arrayObj[1].lastName="Doe2";
     var ret=$filter('orderBy')(arrayObj,{name:'firstName'});//没成功
     console.log(ret);*/
-    var xxx="xxxxxHelloxxxxx";
+   /* var xxx="xxxxxHelloxxxxx";
    $filter('rHello')(xxx);
-    console.log(xxx);
+    console.log(xxx);*/
+     /*
+    console.log(providerServices02);*/
 
+     /*console.log(factoryServices01);
+    console.log(factoryServices02);
 
+   */
+      console.log(providerServices03);
+      console.log(factoryServices03);
+      console.log(serviceServices03);
+      console.log(serviceServices02);
+      providerServices01.setName('李四1111');
+      console.log(providerServices01);
 
+      console.log(providerServices01.getName());
+      console.log(providerServices01.getConfigName());
+      console.log("---------------");
+      serviceServices01.setName("serviceServices01");
+      console.log(serviceServices01.getName());
 
+      serviceServices01.getData()
+       .success(function (data) {
+       console.log("-----getData----success-----");
+       console.log(data);
 
+       }).error(function (err) {
+       console.log("-----getData----error-----");
+       console.log(err);
+       });
 
     $scope.includeFileUr="beInclude.html";
     $scope.tpl="tpl.html";
@@ -63,7 +110,34 @@ app.controller("costController", ["$scope",function($scope) {
 
 
 }]);
+
+app.controller('div1Controller',function($scope){
+    $scope.text='外部页面';
+});
+app.controller('div2Controller',function($scope){
+    $scope.text='div2Controller';
+});
+app.controller('div3Controller',function($scope){
+    $scope.text='div3Controller';
+});
+
+app.controller('div4Controller',['$scope','$routeParams',function($scope,$routeParams){
+    console.log($routeParams);
+    $scope.id=$routeParams.id;
+
+    $scope.text='div4Controller';
+}]);
+
+//module初始化执行操作
 app.run(["$rootScope","$timeout",function ($rootScope,$timeout) {
+    $rootScope.$on("$routeChangeStart", function (event, next, current) {
+
+        console.log("---------$routeChangeStart-----------------");
+        console.log(event);
+        console.log(next);
+        console.log(current);
+        console.log("---------$routeChangeStart--------------end---");
+    });
     $rootScope.name="0";
     var  i=0;
     var exitFlag=false;
